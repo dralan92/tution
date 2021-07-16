d485b68811762f565943a42c25dc5720e01708c5
Alan Sebastian <alansebastian4@gmail.com>
2021-07-16 17:49:58 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{ MediaObserver, MediaChange} from '@angular/flex-layout';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'tution';
+  mediaSub:Subscription = new Subscription();
+  deviceXs:boolean = false;
+  constructor(public mediaObserver:MediaObserver) {
+   
+  }
+
+  ngOnInit(){
+    this.mediaSub = this.mediaObserver.media$.subscribe((result:MediaChange)=>{
+      console.log(result.mqAlias);
+      this.deviceXs = result.mqAlias==='xs'? true:false;
+    });
+  }
+  ngOnDestroy(){
+    this.mediaSub.unsubscribe();
+  } 
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
